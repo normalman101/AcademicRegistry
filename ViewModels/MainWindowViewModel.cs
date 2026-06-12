ea2abe8656173b231d714039eb38a1dec8bc2228
Semyon <normal.man101@outlook.com>
2026-06-12 10:18:02 +0300

    Коммит инициализации

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AcademicRegistry.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AcademicRegistry.ViewModels;
+
+public abstract class ViewModelBase : ReactiveObject
+{
+}
