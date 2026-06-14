63bf674416f1b55fef8af1b99950bc0c3b2815e1
Semyon <normal.man101@outlook.com>
2026-06-14 12:39:48 +0300

    Реализовал сущности

---
+++ Models/Entities/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace AcademicRegistry.Models.Entities;
+
+public record Student(
+    string Name,
+    string Surname,
+    string Patronymic,
+    ImmutableList<Subject> Subjects
+)
+{
+    public Guid Id { init; get; } = Guid.CreateVersion7();
+}
---
+++ Models/Entities/Subject.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AcademicRegistry.Models.Entities;
+
+public record Subject(
+    string Name
+)
+{
+    public Guid Id { init; get; } = Guid.CreateVersion7();
+}
