01c02ac06ee0706f9e4463ec0108f99bd02a3521
Semyon <normal.man101@outlook.com>
2026-06-14 12:41:50 +0300

    Реализовал расширение для наблюдаемой колекции

---
+++ Models/Utilities/ObservableCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace AcademicRegistry.Models.Utilities;
+
+public static class ObservableCollectionExtensions
+{
+    public static void UpdateWith<T>(this ObservableCollection<T> observableCollection, IEnumerable enumerable)
+    {
+        observableCollection.Clear();
+
+        foreach (T element in enumerable) observableCollection.Add(element);
+    }
+}
