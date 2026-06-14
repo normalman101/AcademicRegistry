7eb12182e65c46d1ff5cd86b52b7c8668f133d2c
Semyon <normal.man101@outlook.com>
2026-06-14 12:40:03 +0300

    Реализовал класс для валидации сущностей

---
+++ Models/Utilities/Validator.cs
@@ -0,0 +1,16 @@
+﻿using AcademicRegistry.Models.Entities;
+
+namespace AcademicRegistry.Models.Utilities;
+
+public static class Validator
+{
+    public static bool IsStudentValid(Student student)
+    {
+        return student.Name.Length != 0 && student.Surname.Length != 0 && student.Patronymic.Length != 0;
+    }
+
+    public static bool IsSubjectValid(Subject subject)
+    {
+        return subject.Name.Length != 0;
+    }
+}
